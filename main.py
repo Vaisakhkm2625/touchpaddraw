915d42354474c420d0c4fa51506a0d99b06eca67
MeexReay <meexreay@gmail.com>
2025-03-24 19:40:53 +0300

    dotool patch

--- main.py
+++ main.py
@@ -1,71 +1,51 @@
-# ref: https://askubuntu.com/questions/1345561/how-can-i-get-absolute-touchpad-coordinates
-#1224 804 -> 20 15
-
-import math
-import os
-import time
-
-#get trackpad absolute coords
 from evdev import InputDevice
- 
 import argparse
- 
-from pynput import mouse
-from pynput.mouse import Button
- 
-# Initialize parser
+import os
+import threading
+
 parser = argparse.ArgumentParser()
  
-# Adding optional argument
 parser.add_argument("-d", "--device", help = "device (event*)")
- 
-# Read arguments from command line
+parser.add_argument("-W", "--touchpad-width")
+parser.add_argument("-H", "--touchpad-height")
+
 args = parser.parse_args()
 
-#SET THIS TO YOUR DEVICE
-device = InputDevice('/dev/input/'+ args.device if args.device else 'event7')
+device = InputDevice('/dev/input/' + (args.device or "event7"))
 
-touchpad_x_max = 1224
-touchpad_y_max = 804
+max_tx = args.touchpad_width or 1744
+max_ty = args.touchpad_height or 1266
 
-max_x = 1920
-max_y = 1080
+tx = None
+ty = None
 
-x = 0
-y = 0
+prev_sx = None
+prev_sy = None
 
-def get_xy_coords(e):
-    #you may need to change this number here; i don't know
-    if e.code == 53:
-        global x
-        x = e.value
-    #this one too
-    if e.code == 54:
-        global y
-        y = e.value
-        
-def mapFromTo(x,a,b,c,d):
-   # y=(x-a)//(b-a)*(d-c)+c
-   y=(x-a)/(b-a)*(d-c)+c
-   return y
+device.grab()
 
-x_pos =0
-y_pos =0
+threading.Thread(
+    target = lambda: os.system("dotoold"),
+    daemon = True
+).start()
 
-mouse_controller = mouse.Controller()
+def send_mouse(prev_x, prev_y, x, y):
+    os.system(f"echo \"mouseto {x} {y}\" | dotoolc".encode())
+
+def send_thread_mouse(*args):
+    threading.Thread(
+        target = send_mouse,
+        args = args,
+        daemon = True
+    ).start()
+
 for event in device.read_loop():
-    #rows, cols = stdscr.getmaxyx()
-    get_xy_coords(event)
+    if event.code == 53:
+        tx = event.value
     if event.code == 54:
-        prev_x_pos = x_pos 
-        prev_y_pos = y_pos 
-        x_pos =math.floor(mapFromTo(x,0,touchpad_x_max,0,max_x))
-        y_pos =math.floor(mapFromTo(y,0,touchpad_y_max,0,max_y))
-        if (abs(prev_x_pos-x_pos)>15 or abs(prev_y_pos-y_pos)>15):
-            mouse_controller.release(Button.left)
-            mouse_controller.position = (x_pos,y_pos);
-        mouse_controller.press(Button.left)
-        mouse_controller.position = (x_pos,y_pos);
+        ty = event.value
 
-      #stdscr.addstr(math.floor(mapFromTo(y,0,touchpad_y_max,0,max_y)),math.floor(mapFromTo(x,0,touchpad_x_max,0,max_x)),char)
-
+        sx = tx/max_tx
+        sy = ty/max_ty
+    
+        send_thread_mouse(prev_sx, prev_sy, sx, sy)
